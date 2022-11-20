66a280eda2f1ae83f22de5aa89b972260e8d98fb
Kolyvanov <Kolyvanovsl80@mail.ru>
2022-11-20 23:17:54 +0300

    BooleanSearshEngine realized

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,48 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private Map<String, List<PageEntry>> index = new HashMap<>();
+
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        File[] pdfFiles = pdfsDir.listFiles();
+        for (File pdfFile : pdfFiles) {
+            var doc = new PdfDocument(new PdfReader(pdfFile));
+            for (int i = 1; i <= doc.getNumberOfPages(); i++) {
+                var text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+                var words = (text.toLowerCase().split("\\P{IsAlphabetic}+"));
+
+                Map<String, Integer> freqs = new HashMap<>();
+                for (var word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+                for (var word : freqs.keySet()) {
+                    List<PageEntry> pageEntries = new ArrayList<>();
+                    if (index.containsKey(word)){
+                        pageEntries = index.get(word);
+                    }
+                    pageEntries.add(new PageEntry(pdfFile.getName(), i, freqs.get(word)));
+                    Collections.sort(pageEntries, Collections.reverseOrder());
+                    index.put(word, pageEntries);
+
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return index.get(word.toLowerCase());
+
     }
 }
